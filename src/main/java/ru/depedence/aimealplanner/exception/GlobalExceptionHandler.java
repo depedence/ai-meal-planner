a80db38d41c7db085c82b5bd16c5f5cdc3b0638e
Alexander Averin <a.averin@rosintel.com>
2026-09-01 09:39:08 +0300

    fix GlobalExceptionHandler (add famous annotation)

--- src/main/java/ru/depedence/aimealplanner/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/depedence/aimealplanner/exception/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package ru.depedence.aimealplanner.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(GroqApiException.class)
